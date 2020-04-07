a161f6ecd839219265d7500b8e18c7c0e5b9d003
Tomasz Jakóbiec <tomaszjakobiec94@gmail.com>
2020-04-07 05:32:54 +0200

    remove constructor in Game component as per code review comment

--- index.js
+++ index.js
@@ -44,22 +44,20 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
+    state = {
       history: [{
         squares: Array(9).fill(null)
       }],
       stepNumber: 0,
       xIsNext: true
     };
-  }
 
   handleClick(i) {
     const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
-    if (calculateWinner(squares) || squares[i]) return;
+    if (calculateWinner(squares) || squares[i])
+      return;
 
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
